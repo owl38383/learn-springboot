cdc3786ba52a2feedb76f19c1bc15d0be06d761e
diaochuang <dc2580k@163.com>
2021-03-29 18:07:48 +0800

    Default Changelist

--- learn-boot-consume-rocket/src/main/java/com/aquiver/learnbootrocket/consume/Consumer.java
+++ learn-boot-consume-rocket/src/main/java/com/aquiver/learnbootrocket/consume/Consumer.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
  */
 @Component
 
-public class Customer {
+public class Consumer {
 
     private final Logger logger =  LoggerFactory.getLogger(getClass());
 
